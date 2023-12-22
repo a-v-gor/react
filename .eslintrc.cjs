bf7d119e7bb38801360f9d1ec4c6fd708ae3a1b5
Alexey Gorbenko <a.v.gor@mail.ru>
2023-12-22 14:25:49 +0300

    feat: install all packets to project

---
+++ .eslintrc.cjs
@@ -0,0 +1,59 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es6: true, jest: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      "jsx": true
+    },
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: ['@typescript-eslint', 'react-refresh', 'import', 'react-hooks'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "comma-dangle": ["error", "only-multiline"],
+    "react/prop-types": "off",
+    "react/display-name": "off",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/ban-ts-comment": "error",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-var-requires": "off",
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off"
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
+    },
+    "import/resolver": {
+      "typescript": {
+        "alwaysTryTypes": true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+
+        // use <root>/path/to/folder/tsconfig.json
+        "project": "./",
+      },
+      "node": true
+    },
+    "react": {
+      "pragma": "React",
+      "version": "detect"
+    }
+  }
+}
